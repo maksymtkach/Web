7f11c19ce44136a2e22f105a7d912c45f178d66b
= <=>
2024-04-09 00:06:45 +0200

    Added feature of dynamic lamp-selector

--- Lab5/exercise-1/first.js
+++ Lab5/exercise-1/first.js
@@ -1,129 +1,77 @@
-const bulbs = {
-    incandescent: {
-        canAdjustBrightness: true,
-        temperature: '2700K',
-        minBrightness: 20,
-        maxBrightness: 100
-    },
-    led: {
-        canAdjustBrightness: true,
-        temperature: '5000K', 
-        minBrightness: 30,
-        maxBrightness: 100
-    },
-    saving: {
-        canAdjustBrightness: true,
-        temperature: '3500K',
-        minBrightness: 40,
-        maxBrightness: 100
-    },
-    halogen: {
-        canAdjustBrightness: true,
-        temperature: '3000K',
-        minBrightness: 50,
-        maxBrightness: 100
-    }
-};
-
-['click', 'mousemove', 'keypress', 'scroll'].forEach(event => {
-    document.addEventListener(event, resetInactivityTimer, true);
+document.addEventListener('DOMContentLoaded', () => {
+    initializeLampSelection(); 
+    resetInactivityTimer();
+    setupEventListeners();
 });
 
+const bulbs = {
+    incandescent: { canAdjustBrightness: true, temperature: '2700K', minBrightness: 0, maxBrightness: 100, src: './images/light-lamp.png'},
+    led: { canAdjustBrightness: true, temperature: '5000K', minBrightness: 30, maxBrightness: 100, src: './images/led-lamp.png'},
+    saving: { canAdjustBrightness: true, temperature: '3500K', minBrightness: 40, maxBrightness: 100, src: './images/saving-lamp.png'},
+    halogen: { canAdjustBrightness: true, temperature: '3000K', minBrightness: 50, maxBrightness: 100, src: './images/halogen-lamp.png'}
+};
+let inactivityTimer;
+const bulbTypeRadios = document.querySelectorAll('input[type="radio"][name="lamp-type"]');
 const slider = document.querySelector(".slider");
 const button = document.querySelector(".toggle-button");
-const bulbTypeRadios = document.querySelectorAll('input[type="radio"][name="lamp-type"]');
-let inactivityTimer;
+const lampContainer = document.querySelector('.lamp-container .lamp');
+const temperatureDisplay = document.querySelector(".temperature-display");
 
-function resetInactivityTimer() {
-    clearTimeout(inactivityTimer);
-    inactivityTimer = setTimeout(() => {
-        turnOffLamp();
-    }, 6000);
+function setupEventListeners() {
+    bulbTypeRadios.forEach(radio => {
+        radio.addEventListener('change', () => handleBulbSelection(radio.value));
+    });
+
+    button.addEventListener("click", toggleBulb);
+    slider.addEventListener("input", () => adjustBrightness(lampContainer.classList.contains('light-on')));
+
+    ['click', 'mousemove', 'keypress', 'touchstart'].forEach(eventType => {
+        document.addEventListener(eventType, resetInactivityTimer, false);
+    });
 }
 
-function turnOffLamp() {
-    const selectedLamp = document.querySelector('.lamp.light-on');
-    if (selectedLamp) {
-        selectedLamp.classList.remove('light-on');
-        const lightEffect = selectedLamp.querySelector('.light-effect');
-        if (lightEffect) {
-            lightEffect.style.opacity = 0;
-        }
-        const bulbType = document.querySelector('input[type="radio"][name="lamp-type"]:checked').value;
-        slider.value = bulbs[bulbType].minBrightness;
-    }
-    resetInactivityTimer();
+function initializeLampSelection() {
+    const defaultLampType = document.querySelector('input[type="radio"][name="lamp-type"]:checked').value;
+    handleBulbSelection(defaultLampType);
+}
+
+function handleBulbSelection(selectedBulbType) {
+    lampContainer.className = 'lamp ' + selectedBulbType;
+    const lampImage = lampContainer.querySelector('#lamp-image');
+    lampImage.src = bulbs[selectedBulbType].src;
+    displayTemperature(selectedBulbType);
+    adjustSlider(bulbs[selectedBulbType]);
 }
 
 function toggleBulb() {
-    const selectedLampType = document.querySelector('input[type="radio"][name="lamp-type"]:checked').value;
-    const lampToToggle = document.querySelector(`.lamp.${selectedLampType}`);
-    const isOn = lampToToggle.classList.contains('light-on');
-
-    if (isOn) {
-        turnOffLamp();
-    } else {
-        lampToToggle.classList.add('light-on');
-        const minBrightness = bulbs[selectedLampType].minBrightness;
-        const maxBrightness = bulbs[selectedLampType].maxBrightness;
-        const midBrightness = minBrightness + (maxBrightness - minBrightness) * 0.5;
-        slider.value = midBrightness; 
-        adjustBrightness();
-    }
-    resetInactivityTimer();
+    lampContainer.classList.toggle('light-on');
+    adjustBrightness(lampContainer.classList.contains('light-on'));
 }
 
-bulbTypeRadios.forEach(radio => {
-    radio.addEventListener('change', () => {
-        handleBulbSelection.call(radio);
-        resetInactivityTimer();
-    });
-});
-
-button.addEventListener("click", toggleBulb);
-slider.addEventListener("input", adjustBrightness);
-
-function handleBulbSelection() {
-    const bulbType = this.value;
-    displayBulbImage(bulbType);
-    displayTemperature(bulbType);
-    adjustSlider(bulbs[bulbType]);
-}
-
-function displayBulbImage(bulbType) {
-    document.querySelectorAll('.lamp').forEach(lamp => {
-        lamp.style.display = 'none';
-    });
-    const selectedLamp = document.querySelector(`.lamp.${bulbType}`);
-    if (selectedLamp) {
-        selectedLamp.style.display = 'block';
-    }
-}
 function displayTemperature(bulbType) {
-    const temperatureText = `Temperature: ${bulbs[bulbType].temperature}`;
-    document.querySelector(".temperature-display").innerText = temperatureText;
+    temperatureDisplay.innerText = `Temperature: ${bulbs[bulbType].temperature}`;
 }
 
 function adjustSlider(bulb) {
-    if (bulb.canAdjustBrightness) {
-        slider.disabled = false;
-        slider.min = bulb.minBrightness;
-        slider.max = bulb.maxBrightness;
-        slider.value = bulb.minBrightness;
-    } else {
-        slider.disabled = true;
-    }
+    slider.disabled = !bulb.canAdjustBrightness;
+    slider.min = bulb.minBrightness;
+    slider.max = bulb.maxBrightness;
+    slider.value = bulb.minBrightness;
+    adjustBrightness(lampContainer.classList.contains('light-on'));
 }
 
-function adjustBrightness() {
-    const brightness = slider.value;
-    const selectedLampType = document.querySelector('input[type="radio"][name="lamp-type"]:checked').value;
-    const lightEffect = document.querySelector(`.lamp.${selectedLampType} .light-effect`);
-    if (lightEffect) {
-        lightEffect.style.opacity = brightness / 100;
-    }
+function adjustBrightness(isLightOn) {
+    const brightness = isLightOn ? slider.value : 0;
+    const lightEffect = lampContainer.querySelector('.light-effect');
+    lightEffect.style.opacity = brightness / 100;
 }
 
-resetInactivityTimer();
+function resetInactivityTimer() {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = setTimeout(turnOffLampAutomatically, 10000);
+}
 
-    
+function turnOffLampAutomatically() {
+    lampContainer.classList.remove('light-on');
+    adjustBrightness(false);
+}
